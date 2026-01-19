511dae1e386345ffd48b371bd1429d4dfbc8d048
asus <asus@DESKTOP-ULF63H8.mshome.net>
2026-01-19 22:57:19 +0100

    pls

--- JavaNetflixProject/src/main/java/module-info.java
+++ JavaNetflixProject/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.JavaProject.JavaNetflixProject {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens org.JavaProject.JavaNetflixProject to javafx.fxml;
     exports org.JavaProject.JavaNetflixProject;
