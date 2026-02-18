16724ffecfb1b16d4e297d44479986ebe09c06cc
THINPAD <THINPAD@DESKTOP-H9KUGI0>
2026-02-18 18:54:27 +0100

    database v3.0

---
+++ JavaNetflixProject/src/main/resources/DataBase/database v3.0.sql
@@ -0,0 +1,402 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 18, 2026 at 06:48 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database`
+--
+CREATE DATABASE IF NOT EXISTS `database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `database`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actor`
+--
+
+CREATE TABLE `actor` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `birth_date` date NOT NULL,
+  `nationality` varchar(100) NOT NULL,
+  `photo_url` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+(1, 'action'),
+(2, 'comedy'),
+(3, 'drama'),
+(4, 'fantasy'),
+(5, 'horror'),
+(6, 'romance'),
+(7, 'sci-fi'),
+(8, 'thriller'),
+(9, 'documentary');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `episode`
+--
+
+CREATE TABLE `episode` (
+  `id` int(11) NOT NULL,
+  `season_id` int(11) NOT NULL,
+  `episode_number` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `video_url` text NOT NULL,
+  `duration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
+--
+
+CREATE TABLE `film` (
+  `id` int(11) NOT NULL,
+  `title` varchar(70) NOT NULL,
+  `description` text NOT NULL,
+  `date_sortie` date NOT NULL,
+  `video_url` text NOT NULL,
+  `cover_url` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film_actor`
+--
+
+CREATE TABLE `film_actor` (
+  `film_id` int(11) NOT NULL,
+  `actor_id` int(11) NOT NULL,
+  `role_name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film_categories`
+--
+
+CREATE TABLE `film_categories` (
+  `media_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film_watch_progress`
+--
+
+CREATE TABLE `film_watch_progress` (
+  `film_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `last_position_seconds` double NOT NULL,
+  `completed` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `season`
+--
+
+CREATE TABLE `season` (
+  `id` int(11) NOT NULL,
+  `series_id` int(11) NOT NULL,
+  `season_number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `series`
+--
+
+CREATE TABLE `series` (
+  `id` int(11) NOT NULL,
+  `title` varchar(70) NOT NULL,
+  `description` varchar(10000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `series_actor`
+--
+
+CREATE TABLE `series_actor` (
+  `series_id` int(11) NOT NULL,
+  `actor_id` int(11) NOT NULL,
+  `role_name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `series_categories`
+--
+
+CREATE TABLE `series_categories` (
+  `series_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `series_watch_progress`
+--
+
+CREATE TABLE `series_watch_progress` (
+  `user_id` int(11) NOT NULL,
+  `episode_id` int(11) NOT NULL,
+  `last_position_seconds` int(11) NOT NULL,
+  `completed` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(35) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password_hash` varchar(260) NOT NULL,
+  `role` enum('USER','ADMIN') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `actor`
+--
+ALTER TABLE `actor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `episode`
+--
+ALTER TABLE `episode`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `season_id` (`season_id`);
+
+--
+-- Indexes for table `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `film_actor`
+--
+ALTER TABLE `film_actor`
+  ADD PRIMARY KEY (`film_id`,`actor_id`),
+  ADD KEY `film_id` (`film_id`),
+  ADD KEY `actor_id` (`actor_id`);
+
+--
+-- Indexes for table `film_categories`
+--
+ALTER TABLE `film_categories`
+  ADD PRIMARY KEY (`media_id`,`category_id`),
+  ADD KEY `media_id` (`media_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `film_watch_progress`
+--
+ALTER TABLE `film_watch_progress`
+  ADD PRIMARY KEY (`film_id`,`user_id`),
+  ADD KEY `film_id` (`film_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `season`
+--
+ALTER TABLE `season`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `series_id` (`series_id`);
+
+--
+-- Indexes for table `series`
+--
+ALTER TABLE `series`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `series_actor`
+--
+ALTER TABLE `series_actor`
+  ADD PRIMARY KEY (`series_id`,`actor_id`),
+  ADD KEY `series_id` (`series_id`),
+  ADD KEY `actor_id` (`actor_id`);
+
+--
+-- Indexes for table `series_categories`
+--
+ALTER TABLE `series_categories`
+  ADD PRIMARY KEY (`series_id`,`category_id`),
+  ADD KEY `series_id` (`series_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `series_watch_progress`
+--
+ALTER TABLE `series_watch_progress`
+  ADD PRIMARY KEY (`user_id`,`episode_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `episode_id` (`episode_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `actor`
+--
+ALTER TABLE `actor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `episode`
+--
+ALTER TABLE `episode`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `film`
+--
+ALTER TABLE `film`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `season`
+--
+ALTER TABLE `season`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `series`
+--
+ALTER TABLE `series`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `episode`
+--
+ALTER TABLE `episode`
+  ADD CONSTRAINT `episode_ibfk_1` FOREIGN KEY (`season_id`) REFERENCES `season` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `film_actor`
+--
+ALTER TABLE `film_actor`
+  ADD CONSTRAINT `film_actor_ibfk_1` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `film_actor_ibfk_2` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `film_categories`
+--
+ALTER TABLE `film_categories`
+  ADD CONSTRAINT `film_categories_ibfk_1` FOREIGN KEY (`media_id`) REFERENCES `film` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `film_categories_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `season`
+--
+ALTER TABLE `season`
+  ADD CONSTRAINT `season_ibfk_1` FOREIGN KEY (`series_id`) REFERENCES `series` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `series_actor`
+--
+ALTER TABLE `series_actor`
+  ADD CONSTRAINT `series_actor_ibfk_1` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `series_actor_ibfk_2` FOREIGN KEY (`series_id`) REFERENCES `series` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `series_categories`
+--
+ALTER TABLE `series_categories`
+  ADD CONSTRAINT `series_categories_ibfk_1` FOREIGN KEY (`series_id`) REFERENCES `series` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `series_categories_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `series_watch_progress`
+--
+ALTER TABLE `series_watch_progress`
+  ADD CONSTRAINT `series_watch_progress_ibfk_1` FOREIGN KEY (`episode_id`) REFERENCES `episode` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
